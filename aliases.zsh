58fed6a44c1a40c8bbb8eb202cb9cb73401abed9
Jakub Theimer <theimer@madne.st>
2021-03-07 17:45:56 +0100

    add aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,32 @@
+# Aliases
+
+## PHP & Laravel aliases
+alias lara="cd ~/Developer/laravel"                     # Open default laravel projects directory
+
+alias a="php artisan"                                   # Run artisan
+alias pamfds="php artisan migrate:fresh --seed"         # Migrate fresh database and seed it
+alias pam="php artisan migrate"                         # Migrate database
+alias cdao="composer dump-autoload -o"                  # Composer dump autoload optimised
+alias cr="composer require"                             # Composer require
+alias ci="composer install"                             # Composer install
+alias cu="composer update"                              # Composer update
+alias t="./vendor/bin/phpunit"                          # Run PHPUnit
+
+## JavaScript aliases
+alias yn="yarn nuxt"                                    # Run nuxt
+alias yrp="yarn run production"                         # Run production
+alias y="yarn"                                          # Run yarn
+
+## Git
+alias grs="git reset --soft HEAD~1"                     # Git reset soft
+
+## Python
+alias pip="pip3"                                        # Use pip3
+alias python="python3"                                  # Use python3
+
+## Files
+alias ll="ls -lGaf"                                     # Better file listing
+
+## ZSH
+alias zshconfig="vim ~/.zshrc"                          # Open zsh config in vim
+alias ohmyzsh="vim ~/.oh-my-zsh"                        # Open oh my zsh config in vim
