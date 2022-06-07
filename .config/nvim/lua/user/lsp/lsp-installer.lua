294f8aa147194b257c2d7c018a02baf8ca29a1ba
Jakub Theimer <jakub@dy.st>
2022-06-08 01:14:37 +0200

    add lsp installer config

---
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,37 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+    return
+end
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+    }
+
+    if server.name == "intelephense" then
+        local intelephense_opts = require("user.lsp.settings.intelephense")
+        opts = vim.tbl_deep_extend("force", intelephense_opts, opts)
+    end
+
+    if server.name == "phpactor" then
+        local phpactor_opts = require("user.lsp.settings.phpactor")
+        opts = vim.tbl_deep_extend("force", phpactor_opts, opts)
+    end
+
+    if server.name == "jsonls" then
+        local jsonls_opts = require("user.lsp.settings.jsonls")
+        opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+    end
+
+    if server.name == "sumneko_lua" then
+        local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+        opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+    end
+
+    -- This setup() function is exactly the same as lspconfig's setup function.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
