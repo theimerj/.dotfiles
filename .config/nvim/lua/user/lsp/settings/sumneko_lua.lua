e09f44b7d8961c2154d5b51fd2ff677efb84cd40
Jakub Theimer <jakub@dy.st>
2022-06-08 01:14:17 +0200

    add lua settings

---
+++ .config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -0,0 +1,17 @@
+return {
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
+            completion = { keywordSnippet = "Disable", },
+            diagnostics = { enable = true, globals = {
+                "vim", "describe", "it", "before_each", "after_each" },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                }
+            }
+        }
+    }
+}
