b32d7dcfcbff0f664f9e96d98c345b781d2a79ab
Jakub Theimer <jakub@dy.st>
2022-06-08 01:14:51 +0200

    add colorscheme config

---
+++ .config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,17 @@
+-- Spaceduck theme
+-- local colorscheme = "spaceduck"
+
+-- Tokyo night theme
+-- local colorscheme = "tokyonight"
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_italic_functions = true
+
+-- Challenger deep theme
+local colorscheme = "challenger_deep"
+vim.g.challenger_deep_termcolors = 1
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
