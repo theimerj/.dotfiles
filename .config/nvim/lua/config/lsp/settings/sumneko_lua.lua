df80defdf43db74c1ff4526ce126a47776ea2704
Jakub Theimer <jakub@dy.st>
2022-07-10 18:58:22 +0200

    update sumneko lua lsp config

--- .config/nvim/lua/config/lsp/settings/sumneko_lua.lua
+++ .config/nvim/lua/config/lsp/settings/sumneko_lua.lua
@@ -1,17 +1,25 @@
 return {
-    settings = {
-        Lua = {
-            runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-            completion = { keywordSnippet = "Disable", },
-            diagnostics = { enable = true, globals = {
-                "vim", "describe", "it", "before_each", "after_each" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                }
-            }
-        }
-    }
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+			completion = { keywordSnippet = "Disable" },
+			diagnostics = {
+				enable = true,
+				globals = {
+					"vim",
+					"describe",
+					"it",
+					"before_each",
+					"after_each",
+				},
+			},
+			workspace = {
+				preloadFileSize = 1000,
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
+		},
+	},
 }
