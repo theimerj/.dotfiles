797636eba4be4789e05543909aa2be55842f9158
Jakub Theimer <jakub@dy.st>
2022-07-11 06:24:59 +0200

    remove old lsp settings

--- .config/nvim/lua/config/lsp/settings/phpactor.lua
+++
@@ -1,8 +0,0 @@
-return {
-    settings = {
-        -- ["language_server_psalm.enabled"] = true,
-        -- ["language_server_psalm.bin"] = "~/.local/share/nvim/lsp_servers/psalm/vendor/bin/psalm",
-        -- ["language_server_phpstan.enabled"] = true,
-        -- ["language_server_phpstan.bin"] = "/Users/theimer/.composer/vendor/bin/phpstan",
-    },
-}
