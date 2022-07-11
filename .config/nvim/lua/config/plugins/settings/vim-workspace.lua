f0216de345c138b39c3d4033f62bc45904486393
Jakub Theimer <jakub@dy.st>
2022-07-11 06:26:23 +0200

    DISABLE AUTOSAVE !!! finally i found it

--- .config/nvim/lua/config/plugins/settings/vim-workspace.lua
+++ .config/nvim/lua/config/plugins/settings/vim-workspace.lua
@@ -1,5 +1,9 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+-- disable autosave
+vim.g.workspace_autosave_always = 0
+vim.g.workspace_autosave = 0
+
 -- Toggle Vim Workspace
 keymap("n", "<Leader>tw", ":ToggleWorkspace<CR>", opts)
