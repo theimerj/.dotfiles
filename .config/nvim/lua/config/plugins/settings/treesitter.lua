5021c422382285aada618b704514878ee8ba98fb
Jakub Theimer <jakub@dy.st>
2022-07-11 06:26:08 +0200

    style fix

--- .config/nvim/lua/config/plugins/settings/treesitter.lua
+++ .config/nvim/lua/config/plugins/settings/treesitter.lua
@@ -1,27 +1,26 @@
-
 -- Treesitter
 require("nvim-treesitter.configs").setup({
-	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = { "c", "lua", "rust", "php", "vue", "typescript" },
+    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = { "c", "lua", "rust", "php", "vue", "javascript", "typescript" },
 
-	-- Install languages synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
+        -- list of language that will be disabled
+        -- disable = { "c", "rust" },
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		-- additional_vim_regex_highlighting = false,
-	},
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        -- additional_vim_regex_highlighting = false,
+    },
 
-	context_commentstring = {
-		enable = true,
-	},
+    context_commentstring = {
+        enable = true,
+    },
 })
