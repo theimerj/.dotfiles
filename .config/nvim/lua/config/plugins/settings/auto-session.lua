d9f43144e3f732a144f2ada9c49f068dac3ed7d2
Jakub Theimer <jakub@dy.st>
2022-07-23 03:55:41 +0200

    update auto session config

--- .config/nvim/lua/config/plugins/settings/auto-session.lua
+++ .config/nvim/lua/config/plugins/settings/auto-session.lua
@@ -1,7 +1,8 @@
 require('auto-session').setup {
-
+    -- auto_save_enabled = true,
+    -- auto_session_create_enabled = false,
+    auto_session_suppress_dirs = { "./vendor/*", "./node_modules/*" },
 }
 
 require('session-lens').setup {
-
 }
