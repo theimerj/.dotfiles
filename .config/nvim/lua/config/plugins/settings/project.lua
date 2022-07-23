78a746f692aab1b867a41bf770b5627e6a620357
Jakub Theimer <jakub@dy.st>
2022-07-23 03:56:13 +0200

    remove project settings

--- .config/nvim/lua/config/plugins/settings/project.lua
+++
@@ -1,8 +0,0 @@
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-
-require("project_nvim").setup {
-    detection_methods = { "lsp", "pattern" },
-    atterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile" },
-    exclude_dirs = { "~/vendor/*", "~/node_modules/*" },
-}
