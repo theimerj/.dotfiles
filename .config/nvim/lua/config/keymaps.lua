83e73c3b4d7e1f36756642e838ce8d9d230faa1b
Jakub Theimer <jakub@dy.st>
2023-03-06 17:01:03 +0700

    remove unused

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Disable arrow keys
-vim.keymap.set("n", "<Left>", ':echo "Use h"')
-vim.keymap.set("n", "<Right>", ':echo "Use l"')
-vim.keymap.set("n", "<Up>", ':echo "Use k"')
-vim.keymap.set("n", "<Down>", ':echo "Use j"')
-
 -- Clipboard
 vim.keymap.set("n", "<D-c>", '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "<D-v>", '"+p', { desc = "Paste from system clipboard" })
