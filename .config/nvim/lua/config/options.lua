10b825c26206cd85ce23bc5d49c591bdec008b34
Jakub Theimer <jakub@dy.st>
2023-03-06 12:05:48 +0700

    update options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,3 +9,6 @@ vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- insert 4 spaces for a tab
 vim.opt.softtabstop = 4 -- On insert use 4 spaces for tab
 vim.opt.shell = "/opt/homebrew/bin/zsh" -- shell to use
+vim.opt.numberwidth = 4 -- set the width of the line numbers
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.opt.number = false -- together with relativenumber set to true, it is aligned to the right, which is nicer
