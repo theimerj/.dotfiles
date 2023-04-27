97dc2df8632347c463702aafd818e07967b6c2c3
Jakub Theimer <jakub@dy.st>
2023-04-27 04:13:18 +0200

    add config, use gpt-4

--- .config/nvim/lua/plugins/chat-gpt.lua
+++ .config/nvim/lua/plugins/chat-gpt.lua
@@ -1,7 +1,6 @@
 -- ChatGPT
 return {
   "jackMort/ChatGPT.nvim",
-  cmd = "ChatGPT",
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
@@ -9,7 +8,36 @@ return {
   },
   config = function()
     require("chatgpt").setup({
-      -- optional configuration
+      openai_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "code-davinci-edit-001",
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      keymaps = {
+        close = { "<C-c>" },
+        submit = "<C-Enter>",
+        yank_last = "<C-y>",
+        yank_last_code = "<C-k>",
+        scroll_up = "<C-u>",
+        scroll_down = "<C-d>",
+        toggle_settings = "<C-o>",
+        new_session = "<C-n>",
+        cycle_windows = "<Tab>",
+        -- in the Sessions pane
+        select_session = "<Space>",
+        rename_session = "r",
+        delete_session = "d",
+      },
     })
   end,
 }
