ee37c20705c8ef094594ff426477dc9372243127
Jakub Theimer <jakub@dy.st>
2023-04-27 04:15:52 +0200

    update treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,30 +5,28 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
   },
   opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, {
-        "php",
-        "phpdoc",
-        "typescript",
-        "tsx",
-        "json",
-        "json5",
-        "jsonc",
-        "python",
-        "rust",
-        "vue",
-        "sql",
-        "yaml",
-      })
-      vim.list_extend(opts, {
-        context_commentstring = {
-          enable = true,
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { "markdown" },
-        },
-      })
-    end
+    vim.list_extend(opts.ensure_installed, {
+      "php",
+      "phpdoc",
+      "typescript",
+      "tsx",
+      "json",
+      "json5",
+      "jsonc",
+      "python",
+      "rust",
+      "vue",
+      "sql",
+      "yaml",
+    })
+    vim.list_extend(opts, {
+      context_commentstring = {
+        enable = true,
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown" },
+      },
+    })
   end,
 }
