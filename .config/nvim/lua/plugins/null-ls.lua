ff14e9124f8162393184e5ddb27c5bbe18ee4d1a
Jakub Theimer <jakub@dy.st>
2023-04-27 04:15:16 +0200

    update null ls config

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,16 +1,17 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  opts = function()
+  opts = function(_, opts)
+    table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
     local nls = require("null-ls")
     return {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         -- nls.builtins.formatting.prettierd,
         nls.builtins.formatting.stylua,
-        nls.builtins.diagnostics.flake8,
         nls.builtins.formatting.pint.with({
-          command = vim.fn.expand("~/.composer/vendor/bin/pint"),
-          prefer_local = vim.fn.expand("./vendor/bin/pint"),
-          extra_args = { "--config", vim.fn.expand("~/.config/pint.json") },
+          command = vim.fn.expand("~/.local/share/nvim/mason/packages/pint/vendor/bin/pint"),
+          -- prefer_local = vim.fn.expand("./vendor/bin/pint"),
+          -- extra_args = { "--config", vim.fn.expand("~/.config/pint.json") },
         }),
       },
     }
