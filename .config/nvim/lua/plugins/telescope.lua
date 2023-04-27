4a50ad9fe3454219f04f9a8410fa81963725f476
Jakub Theimer <jakub@dy.st>
2023-04-27 04:15:47 +0200

    update telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,4 @@
-local actions = require("telescope.actions")
-local builtin = require("telescope.builtin")
-local previewers = require("telescope.previewers")
+local Util = require("lazyvim.util")
 
 return {
   "nvim-telescope/telescope.nvim",
@@ -11,93 +9,100 @@ return {
       config = function()
         require("telescope").load_extension("fzf")
       end,
+      keys = {
+        -- stylua: ignore
+        { "<leader>fh", function() Util.telescope("find_files", { hidden = true })() end, desc = "Find Hidden Files", },
+        -- stylua: ignore
+        { "<leader>fi", function() Util.telescope("find_files", { no_ignore = true })() end, desc = "Find Ignored Files", },
+      },
     },
   },
-  keys = {
-    { "<leader>fr", ":Telescope lsp_document_symbols<cr>", desc = "Find Document Symbols" },
-  },
+
   opts = {
     defaults = {
+      prompt_prefix = " ",
+      selection_caret = " ",
       path_display = { "smart" },
       mappings = {
         i = {
-          ["<C-n>"] = actions.cycle_history_next,
-          ["<C-p>"] = actions.cycle_history_prev,
-
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-
-          ["<C-c>"] = actions.close,
-
-          ["<Down>"] = actions.move_selection_next,
-          ["<Up>"] = actions.move_selection_previous,
-
-          ["<CR>"] = actions.select_default,
-          ["<C-x>"] = actions.select_horizontal,
-          ["<C-v>"] = actions.select_vertical,
-          ["<C-t>"] = actions.select_tab,
-
-          ["<C-u>"] = actions.preview_scrolling_up,
-          ["<C-d>"] = actions.preview_scrolling_down,
-
-          ["<PageUp>"] = actions.results_scrolling_up,
-          ["<PageDown>"] = actions.results_scrolling_down,
-
-          ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-          ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-          ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-          ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          ["<C-l>"] = actions.complete_tag,
-          ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-
-          -- ["<C-t>"] = trouble.open_with_trouble, -- Open with Trouble
+          ["<C-t>"] = function(...)
+            return require("trouble.providers.telescope").open_with_trouble(...)
+          end,
+          ["<A-†>"] = function(...)
+            return require("trouble.providers.telescope").open_selected_with_trouble(...)
+          end,
+          ["<A-i>"] = function()
+            Util.telescope("find_files", { no_ignore = true })()
+          end,
+          ["<A-˙>"] = function()
+            Util.telescope("find_files", { hidden = true })()
+          end,
+          ["<C-Down>"] = function(...)
+            return require("telescope.actions").cycle_history_next(...)
+          end,
+          ["<C-Up>"] = function(...)
+            return require("telescope.actions").cycle_history_prev(...)
+          end,
+          ["<C-f>"] = function(...)
+            return require("telescope.actions").preview_scrolling_down(...)
+          end,
+          ["<C-b>"] = function(...)
+            return require("telescope.actions").preview_scrolling_up(...)
+          end,
+          ["<C-n>"] = function(...)
+            return require("telescope.actions").cycle_history_next(...)
+          end,
+          ["<C-p>"] = function(...)
+            return require("telescope.actions").cycle_history_prev(...)
+          end,
+          ["<C-j>"] = function(...)
+            return require("telescope.actions").move_selection_next(...)
+          end,
+          ["<C-k>"] = function(...)
+            return require("telescope.actions").move_selection_previous(...)
+          end,
         },
 
         n = {
-          ["<esc>"] = actions.close,
-          ["<CR>"] = actions.select_default,
-          ["<C-x>"] = actions.select_horizontal,
-          ["<C-v>"] = actions.select_vertical,
-          ["<C-t>"] = actions.select_tab,
-
-          ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-          ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-          ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-          ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-          ["j"] = actions.move_selection_next,
-          ["k"] = actions.move_selection_previous,
-          ["H"] = actions.move_to_top,
-          ["M"] = actions.move_to_middle,
-          ["L"] = actions.move_to_bottom,
-
-          ["<Down>"] = actions.move_selection_next,
-          ["<Up>"] = actions.move_selection_previous,
-          ["gg"] = actions.move_to_top,
-          ["G"] = actions.move_to_bottom,
-
-          ["<C-u>"] = actions.preview_scrolling_up,
-          ["<C-d>"] = actions.preview_scrolling_down,
-
-          ["<PageUp>"] = actions.results_scrolling_up,
-          ["<PageDown>"] = actions.results_scrolling_down,
-
-          ["?"] = actions.which_key,
-
-          -- ["<C-t>"] = trouble.open_with_trouble, -- Open with Trouble
+          ["p"] = function(...)
+            return require("telescope.actions").close(...)
+          end,
+          ["?"] = function(...)
+            return require("telescope.actions").which_key(...)
+          end,
+          ["t"] = function(...)
+            return require("trouble.providers.telescope").open_selected_with_trouble(...)
+          end,
         },
       },
     },
   },
 }
-
--- vim.keymap.set("n", "<Leader><Leader>", "<cmd>Telescope find_files<cr>", opts)
--- vim.keymap.set("n", "<Leader>fhf", "<cmd>Telescope find_files hidden=true<cr>", opts)
--- vim.keymap.set("n", "<Leader>fr", "<cmd>Telescope lsp_document_symbols<cr>", opts)
--- vim.keymap.set("n", "<Leader>fg", "<cmd>Telescope live_grep<cr>", opts)
--- vim.keymap.set("n", "<Leader>fb", "<cmd>Telescope buffers<cr>", opts)
--- vim.keymap.set("n", "<Leader>fh", "<cmd>Telescope help_tags<cr>", opts)
--- vim.keymap.set("n", "<Leader>fd", "<cmd>Telescope diagnostics<cr>", opts)
--- vim.keymap.set("n", "<Leader>fs", "<cmd>Telescope session-lens search_session<cr>", opts)
--- vim.keymap.set("n", "<Leader>ft", "<cmd>TodoTelescope<cr>", opts)
--- vim.keymap.set("n", "<Leader>ff", "<cmd>Telescope file_browser<cr>", opts)
+-- ["<C-n>"] = actions.cycle_history_next,
+-- ["<C-p>"] = actions.cycle_history_prev,
+--
+-- ["<C-j>"] = actions.move_selection_next,
+-- ["<C-k>"] = actions.move_selection_previous,
+--
+-- ["<C-c>"] = actions.close,
+--
+-- ["<Down>"] = actions.move_selection_next,
+-- ["<Up>"] = actions.move_selection_previous,
+--
+-- ["<CR>"] = actions.select_default,
+-- ["<C-x>"] = actions.select_horizontal,
+-- ["<C-v>"] = actions.select_vertical,
+-- ["<C-t>"] = actions.select_tab,
+--
+-- ["<C-u>"] = actions.preview_scrolling_up,
+-- ["<C-d>"] = actions.preview_scrolling_down,
+--
+-- ["<PageUp>"] = actions.results_scrolling_up,
+-- ["<PageDown>"] = actions.results_scrolling_down,
+--
+-- ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+-- ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+-- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+-- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+-- ["<C-l>"] = actions.complete_tag,
+-- ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
