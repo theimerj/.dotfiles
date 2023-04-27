8c181e16e0e0bee3326db27824f0bf57e827bfc4
Jakub Theimer <jakub@dy.st>
2023-04-27 04:15:59 +0200

    add whichkey config

---
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    defaults = {
+      ["<leader>t"] = { name = "+test" },
+      ["<leader>d"] = { name = "+debug" },
+      ["<leader>cp"] = { name = "+php" },
+      ["<leader>o"] = { name = "+obsidian" },
+      ["<leader>a"] = { name = "+annotations/docblocks" },
+    },
+  },
+}
