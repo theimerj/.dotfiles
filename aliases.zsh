68ca79f2f3d9950742a0508ff33d7568ab37b23a
Jakub Theimer <jakub@dy.st>
2023-04-27 04:16:30 +0200

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -8,55 +8,57 @@ alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
 # Directories
-alias dot="cd $DOTFILES"                                # Go to .dotfiles directory
-alias repos="cd ~/Developer"                            # Open default projects directory
-alias lara="cd ~/Developer/php"                         # Open default php / laravel projects directory
-alias obs="cd $OBSIDIAN"                                # Go to obsidian root directory
+alias dot="cd $DOTFILES"                                    # Go to .dotfiles directory
+alias repos="cd ~/Developer"                                # Open default projects directory
+alias lara="cd ~/Developer/php"                             # Open default php / laravel projects directory
+alias obs="cd $OBSIDIAN"                                    # Go to obsidian root directory
 
 ## PHP & Laravel aliases
-alias a="php artisan"                                   # Run artisan
-alias pamfds="php artisan migrate:fresh --seed"         # Migrate fresh database and seed it
-alias pam="php artisan migrate"                         # Migrate database
-alias pas="php artisan seed"                            # Seed database
-alias cdao="composer dump-autoload -o"                  # Composer dump autoload optimised
-alias cr="composer require"                             # Composer require
-alias ci="composer install"                             # Composer install
-alias cu="composer update"                              # Composer update
-alias t="./vendor/bin/pest"                             # Run Pest tests
+alias a="php artisan"                                       # Run artisan
+alias pamfds="php artisan migrate:fresh --seed"             # Migrate fresh database and seed it
+alias pam="php artisan migrate"                             # Migrate database
+alias pas="php artisan seed"                                # Seed database
+alias cdao="composer dump-autoload -o"                      # Composer dump autoload optimised
+alias cr="composer require"                                 # Composer require
+alias ci="composer install"                                 # Composer install
+alias cu="composer update"                                  # Composer update
+alias t="./vendor/bin/pest"                                 # Run Pest tests
 alias pt="./vendor/bin/phpunit"                             # Run PHPUnit tests
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'   # Run Laravel Sail
 
 ## JavaScript aliases
-alias yn="yarn nuxt"                                    # Run nuxt
-alias yrp="yarn run production"                         # Run production
-alias yrp="yarn run watch"                              # Run watch
-alias y="yarn"                                          # Run yarn
+alias yn="yarn nuxt"                                        # Run nuxt
+alias yrp="yarn run production"                             # Run production
+alias yrp="yarn run watch"                                  # Run watch
+alias y="yarn"                                              # Run yarn
 
 ## Git
-alias grs="git reset --soft HEAD~1"                     # Git reset soft
-alias gst="git status"                                  # Git status
-alias gb="git branch"                                   # Git branch
-alias gc="git checkout"                                 # Git checkout
-alias gl="git log --oneline --decorate --color"         # Git log
-alias amend="git add . && git commit --amend --no-edit" # Git ament
-alias commit="git add . && git commit -m"               # Git commit
-alias diff="git diff"                                   # Git diff
-alias pull="git pull"                                   # Git pull
-alias push="git push"                                   # Git push
-alias unstage="git restore --staged ."                  # Git unstage
-alias wip="commit wip"                                  # Git commit wip
+alias grs="git reset --soft HEAD~1"                         # Git reset soft
+alias gst="git status"                                      # Git status
+alias gb="git branch"                                       # Git branch
+alias gc="git checkout"                                     # Git checkout
+alias gl="git log --oneline --decorate --color"             # Git log
+alias amend="git add . && git commit --amend --no-edit"     # Git ament
+alias commit="git add . && git commit -m"                   # Git commit
+alias diff="git diff"                                       # Git diff
+alias pull="git pull"                                       # Git pull
+alias push="git push"                                       # Git push
+alias unstage="git restore --staged ."                      # Git unstage
+alias wip="commit wip"                                      # Git commit wip
 
 ## Python
-# alias pip="pip3"                                      # Use pip3
-# alias python="python3"                                # Use python3
+# alias pip="pip3"                                          # Use pip3
+# alias python="python3"                                    # Use python3
 
 ## Files
-alias ll="exa -la"                                     # Better file listing
+alias ll="exa -la"                                          # Better file listing
 
 ## Vim
-alias vim="nvim"                                        # Use nvim
-alias neo="neovide"                         # Use neovide
-alias vimr="vimr"                                       # Use vimr
+alias vim="nvim"                                            # Use nvim
+alias neo="neovide"                                         # Use neovide
+alias vimr="vimr"                                           # Use vimr
 
 ## ZSH
-alias zshconfig="vim ~/.zshrc"                          # Open zsh config in vim
-alias ohmyzsh="vim ~/.oh-my-zsh"                        # Open oh my zsh config in vim
+alias zshconfig="vim ~/.zshrc"                              # Open zsh config in vim
+alias ohmyzsh="vim ~/.oh-my-zsh"                            # Open oh my zsh config in vim
+
