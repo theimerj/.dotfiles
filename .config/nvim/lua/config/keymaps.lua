0deefc0425f170b54d8829f57b1e8920c25cff6b
Jakub Theimer <jakub@dy.st>
2023-06-01 08:54:44 +0200

    add lazydocker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,9 @@ vim.keymap.set("n", "<Leader>.", ":bd<CR>", { desc = "Close Buffer" })
 
 -- Quick settings access
 vim.keymap.set("n", "<Leader>C", ":edit /Users/theimer/.config/nvim/init.lua<CR>", { desc = "Edit Configuration" })
+
+-- Lazydocker
+-- stylua: ignore
+vim.keymap.set("n", "<leader>gd", function() require("lazyvim.util").float_term({ "lazydocker" }, { cwd = require("lazyvim.util").get_root(), esc_esc = false }) end, { desc = "Lazydocker (root dir)" })
+-- stylua: ignore
+vim.keymap.set("n", "<leader>gD", function() require("lazyvim.util").float_term({ "lazydocker" }, {esc_esc = false}) end, { desc = "Lazydocker (cwd)" })
