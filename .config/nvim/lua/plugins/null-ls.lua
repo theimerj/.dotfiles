50d3a5e2ddd9c2f052bf4f3ff87705b70b7bbc78
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-08-03 11:46:53 +0200

    update null ls config

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -6,15 +6,24 @@ return {
     return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
-        -- nls.builtins.formatting.prettierd,
+        nls.builtins.code_actions.gitsigns,
+        -- nls.builtins.formatting.prettierd.with({
+        --   condition = function(utils)
+        --     return utils.root_has_file({ ".prettierrc.json" })
+        --   end,
+        -- }),
+        nls.builtins.formatting.rustywind,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.xmlformat,
         nls.builtins.formatting.pint.with({
-          command = vim.fn.expand("~/.local/share/nvim/mason/packages/pint/vendor/bin/pint"),
+          command = vim.fn.expand("/Users/theimer/.local/share/nvim/mason/packages/pint/vendor/bin/pint"),
           -- prefer_local = vim.fn.expand("./vendor/bin/pint"),
           -- extra_args = { "--config", vim.fn.expand("~/.config/pint.json") },
         }),
+        nls.builtins.diagnostics.phpstan,
+        nls.builtins.diagnostics.shellcheck,
       },
+      temp_dir = "/tmp",
     }
   end,
 }
