9c489c9c8501dcbd2ad3a62e3feec993eea59447
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-09-19 01:24:17 +0200

    add autotag

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,10 @@ return {
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
     "nvim-treesitter/nvim-treesitter-context",
+    "windwp/nvim-ts-autotag",
   },
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "php",
       "phpdoc",
       "typescript",
@@ -18,15 +19,19 @@ return {
       "vue",
       "sql",
       "yaml",
-    })
-    vim.list_extend(opts, {
-      context_commentstring = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "markdown" },
-      },
-    })
-  end,
+    },
+    context_commentstring = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "markdown" },
+    },
+  },
 }
