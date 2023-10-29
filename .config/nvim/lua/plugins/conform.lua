5bca23c4ddbba28f912a39fe9ae139dd13e8fb33
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-29 02:11:55 +0200

    add conform (instead of null-ls)

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  opts = {
+    formatters_by_ft = {
+      css = { "prettierd" },
+      html = { "prettierd" },
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      json = { "prettierd" },
+      lua = { "stylua" },
+      markdown = { "prettierd" },
+      php = { "pint" },
+      svelte = { "prettier" },
+      typescript = { "prettierd" },
+      yaml = { "prettier" },
+    },
+  },
+}
