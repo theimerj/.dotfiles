403e2640d1a28eab9a107f5bda00ad8184d1ba0b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-29 02:12:55 +0200

    add nvim-link config

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = "LazyFile",
+  opts = {
+    -- Event to trigger linters
+    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+    linters_by_ft = {
+      -- php = { "phpcs" },
+      -- Use the "*" filetype to run linters on all filetypes.
+      -- ['*'] = { 'global linter' },
+      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+      -- ['_'] = { 'fallback linter' },
+    },
+    -- LazyVim extension to easily override linter options
+    -- or add custom linters.
+    ---@type table<string,table>
+    linters = {
+      --
+    },
+  },
+}
