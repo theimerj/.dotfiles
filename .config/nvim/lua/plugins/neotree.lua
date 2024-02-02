6a4c86165fe5914e6505b80fe93de3eaf4cbe032
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-02 16:27:51 +0100

    update neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,9 +2,6 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = true,
-      use_libuv_file_watcher = true,
       hijack_netrw_behavior = "open_default", -- "open_default" or "open_current" or "disabled",
     },
   },
