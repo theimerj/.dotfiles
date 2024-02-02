9d4a47924144b2986bd5bfe512a24dc011dc611c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-02 16:26:48 +0100

    remove catpuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,58 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      term_colors = true,
-      transparent_background = false,
-      integrations = {
-        cmp = true,
-        dashboard = true,
-        gitsigns = true,
-        harpoon = false,
-        lsp_trouble = false,
-        markdown = true,
-        mason = true,
-        neotest = true,
-        noice = false,
-        notify = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = false,
-        which_key = false,
-        dap = {
-          enabled = false,
-          enable_ui = false,
-        },
-        indent_blankline = {
-          enabled = true,
-          colored_indent_levels = false,
-        },
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
-        },
-      },
-    },
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = false,
@@ -62,14 +9,18 @@ return {
       variant = "auto",
       --- @usage 'main'|'moon'|'dawn'
       dark_variant = "main",
-      disable_float_background = false,
+      enable = {
+        legacy_highlights = false,
+        migrations = false,
+        terminal = true,
+      },
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
       highlight_groups = {
-        TabLineFill = { bg = "none" },
-        MiniIndentscopeSymbol = { fg = "iris" },
-        MiniIndentscopeSymbolOff = { fg = "iris" },
-        IndentBlanklineChar = { fg = "overlay" },
-        IndentBlanklineSpaceChar = { fg = "overlay" },
-        IndentBlanklineSpaceCharBlankline = { fg = "overlay" },
+        --
       },
     },
   },
