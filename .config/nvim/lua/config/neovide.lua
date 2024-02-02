ada3620d44bcac89cbc43a61d7be80fd472503b8
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-02 16:26:10 +0100

    update neovide

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -3,18 +3,17 @@ if vim.g.neovide then
   vim.opt.guifont = { "DankMono Nerd Font Mono", ":h20" }
   -- vim.opt.linespace = 4
 
-  -- Helper function for transparency formatting
-  local alpha = function()
-    return string.format("%x", math.floor(255 * 0.95))
-  end
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.90
-  vim.g.neovide_background_color = "#191723" .. alpha()
+  -- vim.g.neovide_transparency = 0.0
+  -- vim.g.transparency = 0.90
+  -- vim.g.neovide_background_color = "#191723" .. alpha()
 
-  -- Light theme settings
-  -- vim.opt.background = "light"
-  -- vim.g.neovide_background_color = "#ffffff" .. alpha()
+  -- The transparency rendering on macOS has been changed, and a full fix is work in progress.
+  vim.g.neovide_transparency = 0.85
+  vim.g.neovide_window_blurred = true
+
+  -- vim.g.neovide_remember_window_size = false
+
+  vim.g.neovide_theme = "auto"
 
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
@@ -23,7 +22,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
 
   -- Set scroll animation length
-  vim.g.neovide_scroll_animation_length = 0.2
+  vim.g.neovide_scroll_animation_length = 0.125
 
   -- Enable cursor ripple effect
   vim.g.neovide_cursor_vfx_mode = "ripple"
