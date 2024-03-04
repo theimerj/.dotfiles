e8b5945b086908efc19a61db97583bd65db966b8
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-04 23:10:42 +0100

    add blade support to treesitter
    add folds

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,9 +7,6 @@ return {
       "html",
       "javascript",
       "jsdoc",
-      "json",
-      "json5",
-      "jsonc",
       "lua",
       "luadoc",
       "luap",
@@ -42,4 +39,24 @@ return {
       additional_vim_regex_highlighting = { "markdown" },
     },
   },
+  config = function(_, opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+
+    vim.filetype.add({
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    })
+
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
