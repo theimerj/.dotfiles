27172415da9c55fe6d3ed63c99af42f165bd976f
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-09 04:40:10 +0100

    set winblend to 25 and scrolloff to 16

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,5 +12,6 @@ vim.opt.shell = "/opt/homebrew/bin/zsh" -- shell to use
 vim.opt.numberwidth = 4 -- set the width of the line numbers
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 vim.opt.number = false -- together with relativenumber set to true, it is aligned to the right, which is nicer
-vim.opt.winblend = 0 -- global transparency for floating windows
+vim.opt.winblend = 25 -- global transparency for floating windows
 vim.opt.conceallevel = 2 -- level of text hiding
+vim.opt.scrolloff = 16 -- minimum number of screen lines to keep above and below the cursor
