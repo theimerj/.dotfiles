39940c10339327813501aab18e761831455f0549
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-09 04:42:15 +0100

    add chat-gpt group in whichkey

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -5,6 +5,7 @@ return {
       ["<leader>t"] = { name = "+test" },
       ["<leader>d"] = { name = "+debug" },
       ["<leader>cp"] = { name = "+php" },
+      ["<leader>cg"] = { name = "+chat-gpt" },
       ["<leader>o"] = { name = "+obsidian" },
       ["<leader>a"] = { name = "+annotations/docblocks" },
       ["<leader>p"] = { name = "+pomodoro" },
