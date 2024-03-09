2464a8fd6590c7edb4df9d57c1c68c178055a2b2
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-09 04:42:22 +0100

    update highlights

--- .config/nvim/queries/blade/highlights.scm
+++ .config/nvim/queries/blade/highlights.scm
@@ -1,7 +1,7 @@
 (directive) @function
 (directive_start) @function
 (directive_end) @function
-(comment) @comment
+(comment) @comment @spell
 ((parameter) @include (#set! "priority" 110))
 ((php_only) @include (#set! "priority" 110))
 ((bracket_start) @function (#set! "priority" 120))
