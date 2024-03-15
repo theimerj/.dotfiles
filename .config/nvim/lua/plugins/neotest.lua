43ed3d77f0001692e2873f4bbce6f7fd417650d3
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-15 21:34:39 +0100

    finally working neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,14 +1,67 @@
+-- NOTE: https://github.com/V13Axel/neotest-pest
 return {
   {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/neotest-plenary",
-      "theutz/neotest-pest",
+      "V13Axel/neotest-pest",
     },
     opts = {
+      log_level = vim.log.levels.DEBUG,
       adapters = {
-        "neotest-plenary",
-        "neotest-pest",
+        -- "neotest-plenary",
+        ["neotest-pest"] = {
+          -- Root files
+          root_files = { "tests/Pest.php" },
+
+          -- Ignore these directories when looking for tests
+          ignore_dirs = { "vendor", "node_modules" },
+
+          -- Ignore any projects containing "phpunit-only.tests"
+          root_ignore_files = {},
+
+          -- Specify suffixes for files that should be considered tests
+          test_file_suffixes = { "Test.php" },
+
+          -- Enable Docker support
+          sail_enabled = function()
+            return true
+          end,
+
+          -- vim.fn.fnamemodify(require("lazyvim.util").root.get({ normalize = true }), ":h:t") .. "-api-fpm-1",
+
+          -- WARNING: Not used?
+          -- Docker executable
+          sail_executable = {
+            "docker",
+            "compose",
+            "-f",
+            "../docker-compose.dev.yml",
+            "exec",
+            "api-fpm",
+          },
+
+          -- Custom pest binary.
+          pest_cmd = {
+            "docker",
+            "compose",
+            "-f",
+            "../docker-compose.dev.yml",
+            "exec",
+            "api-fpm",
+            "vendor/bin/pest",
+          },
+
+          -- Run N tests in parallel, <=1 doesn't pass --parallel to pest at all
+          parallel = 0,
+
+          -- Enable ["compact" output printer](https://pestphp.com/docs/optimizing-tests#content-compact-printer)
+          compact = false,
+
+          results_path = function()
+            return "storage/app/tests/" .. os.date("pest-%Y%m%d-%H%M%S")
+          end,
+        },
       },
     },
   },
