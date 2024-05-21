39388fd1c2e29b95e2e9bc6de1dd3d888d0a56a4
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-21 08:10:02 +0200

    update markdown preview config

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -4,14 +4,6 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
-  keys = {
-    {
-      "<leader>mp",
-      ft = "markdown",
-      "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "Markdown Preview",
-    },
-  },
   config = function()
     vim.cmd([[do FileType]])
   end,
