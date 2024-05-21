16cf55ad404f05b882d02d4d54614ea152deb931
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-21 08:10:21 +0200

    update phpactor config

--- .config/nvim/lua/plugins/phpactor.lua
+++ .config/nvim/lua/plugins/phpactor.lua
@@ -29,7 +29,7 @@ return {
         -- php_bin = "php",
         -- composer_bin = "composer",
         -- git_bin = "git",
-        check_on_startup = "always",
+        check_on_startup = "daily",
       },
       lspconfig = {
         options = {
@@ -39,26 +39,3 @@ return {
     },
   },
 }
-
--- return {
---   {
---     "phpactor/phpactor",
---     ft = "php",
---     build = "composer install --no-dev -o",
---     keys = {
---       { "<leader>cpi", ":PhpactorImportClass<CR>", mode = { "n" }, desc = "Import Class" },
---       { "<leader>cpn", ":PhpactorClassNew<CR>", desc = "New Class" },
---       { "<leader>cpv", ":PhpactorChangeVisibility<CR>", mode = { "n" }, desc = "Change Visibility" },
---       { "<leader>cpe", ":PhpactorClassExpand<CR>", desc = "Expand Class" },
---       { "<leader>cpt", ":PhpactorTransform<CR>", mode = { "n" }, desc = "Transform Class" },
---       { "<leader>cpc", ":PhpactorCopyClassName<CR>", mode = { "n" }, desc = "Copy Class Name" },
---       { "<leader>cpm", ":PhpactorMoveFile<CR>", mode = { "n" }, desc = "Move File" },
---       { "<leader>cpe", ":PhpactorExtractMethod<CR>", mode = { "v" }, desc = "Extract Method" },
---       { "<leader>cpe", ":PhpactorExtractExpression<CR>", mode = { "v" }, desc = "Extract Expression" },
---       { "<leader>cpi", ":PhpactorImportMissingClasses<CR>", mode = { "v" }, desc = "Import Missing Classes" },
---     },
---     config = function()
---       --
---     end,
---   },
--- }
