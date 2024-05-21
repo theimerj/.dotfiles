be4782f321bf4cf4d9242894ce1827f317d1e103
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-21 08:09:40 +0200

    update disabled plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/noice.nvim",        enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { " iamcco/markdown-preview.nvim", enabled = false },
 }
