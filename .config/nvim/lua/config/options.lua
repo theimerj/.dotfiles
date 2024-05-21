7abed6c53be496f7a9e907ab56a9eb404590f319
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-21 16:09:33 +0200

    set Lazyvim shell

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,7 +8,6 @@ vim.g.maplocalleader = ","
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- insert 4 spaces for a tab
 vim.opt.softtabstop = 4 -- On insert use 4 spaces for tab
-vim.opt.shell = "/opt/homebrew/bin/zsh" -- shell to use
 vim.opt.numberwidth = 4 -- set the width of the line numbers
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 vim.opt.number = false -- together with relativenumber set to true, it is aligned to the right, which is nicer
@@ -19,3 +18,5 @@ vim.opt.scrolloff = 16 -- minimum number of screen lines to keep above and below
 vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup" -- where to store backup files
 vim.opt.directory = vim.fn.stdpath("data") .. "/swap" -- where to store swap files
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo" -- where to store undo files
+
+LazyVim.terminal.setup("/bin/zsh")
