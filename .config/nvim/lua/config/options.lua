b38a25170d260dba00ded56652896156184c6c4c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-06-12 04:28:18 +0200

    comment out custom dir paths

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -15,8 +15,8 @@ vim.opt.winblend = 20 -- global transparency for floating windows
 vim.opt.conceallevel = 2 -- level of text hiding
 vim.opt.scrolloff = 16 -- minimum number of screen lines to keep above and below the cursor
 
-vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup" -- where to store backup files
-vim.opt.directory = vim.fn.stdpath("data") .. "/swap" -- where to store swap files
-vim.opt.undodir = vim.fn.stdpath("data") .. "/undo" -- where to store undo files
+-- vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup" -- where to store backup files
+-- vim.opt.directory = vim.fn.stdpath("data") .. "/swap" -- where to store swap files
+-- vim.opt.undodir = vim.fn.stdpath("data") .. "/undo" -- where to store undo files
 
 LazyVim.terminal.setup("/bin/zsh")
