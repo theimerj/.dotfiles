e45a8ae366cd7be6008215f7d892707df11058c3
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-06-15 01:13:11 +0200

    add zellij

--- commands.zsh
+++ commands.zsh
@@ -1,5 +1,6 @@
 # Commands
 
+# Lazygit
 lg() {
     export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
 
@@ -48,4 +49,19 @@ function stage {
     git push &&
     echo "checkout back to $SOURCE_BRANCH" &&
     git checkout $SOURCE_BRANCH
+}
+
+# Zellij
+function zr () { zellij run --name "$*" -- zsh -ic "$*";}
+function zrf () { zellij run --name "$*" --floating -- zsh -ic "$*";}
+function zri () { zellij run --name "$*" --in-place -- zsh -ic "$*";}
+function ze () { zellij edit "$*";}
+function zef () { zellij edit --floating "$*";}
+function zei () { zellij edit --in-place "$*";}
+function zpipe () {
+  if [ -z "$1" ]; then
+    zellij pipe;
+  else
+    zellij pipe -p $1;
+  fi
 }
