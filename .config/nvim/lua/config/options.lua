648566332b03e782bbc95508de3e693a189e6a3c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-07-09 01:26:34 +0200

    update nvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,15 +5,23 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
-vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.autowrite = false -- Disable auto write
+vim.opt.shiftwidth = 4 -- The number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- Insert 4 spaces for a tab
 vim.opt.softtabstop = 4 -- On insert use 4 spaces for tab
-vim.opt.numberwidth = 4 -- set the width of the line numbers
-vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
-vim.opt.number = false -- together with relativenumber set to true, it is aligned to the right, which is nicer
-vim.opt.winblend = 20 -- global transparency for floating windows
-vim.opt.conceallevel = 2 -- level of text hiding
-vim.opt.scrolloff = 16 -- minimum number of screen lines to keep above and below the cursor
+vim.opt.numberwidth = 4 -- Set the width of the line numbers
+vim.opt.signcolumn = "yes" -- Always show the sign column, otherwise it would shift the text each time
+vim.opt.number = false -- Together with relativenumber set to true, it is aligned to the right, which is nicer
+vim.opt.winblend = 20 -- Global transparency for floating windows
+vim.opt.pumblend = 20 -- Popup blend
+vim.opt.conceallevel = 2 -- Level of text hiding
+vim.opt.scrolloff = 16 -- Minimum number of screen lines to keep above and below the cursor
+
+-- Set filetype to `bigfile` for files larger than 1.5 MB
+-- Only vim syntax will be enabled (with the correct filetype)
+-- LSP, treesitter and other ft plugins will be disabled.
+-- mini.animate will also be disabled.
+vim.g.bigfile_size = 1024 * 1024 * 5 -- 5 MB
 
 -- vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup" -- where to store backup files
 -- vim.opt.directory = vim.fn.stdpath("data") .. "/swap" -- where to store swap files
