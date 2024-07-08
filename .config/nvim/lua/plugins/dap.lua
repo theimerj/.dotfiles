d4c74095c1d5af08ce6d7055fa341afae8e600fb
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-07-09 01:28:09 +0200

    update dap config

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -27,6 +27,7 @@ return {
         },
       }
     end
+
     if not dap.adapters["pwa-node"] then
       require("dap").adapters["pwa-node"] = {
         type = "server",
@@ -43,6 +44,7 @@ return {
         },
       }
     end
+
     if not dap.configurations["php"] then
       dap.configurations["php"] = {
         {
@@ -56,7 +58,28 @@ return {
         },
       }
     end
-    for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+
+    if not dap.adapters["node"] then
+      dap.adapters["node"] = function(cb, config)
+        if config.type == "node" then
+          config.type = "pwa-node"
+        end
+        local nativeAdapter = dap.adapters["pwa-node"]
+        if type(nativeAdapter) == "function" then
+          nativeAdapter(cb, config)
+        else
+          cb(nativeAdapter)
+        end
+      end
+    end
+
+    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+
+    local vscode = require("dap.ext.vscode")
+    vscode.type_to_filetypes["node"] = js_filetypes
+    vscode.type_to_filetypes["pwa-node"] = js_filetypes
+
+    for _, language in ipairs(js_filetypes) do
       if not dap.configurations[language] then
         dap.configurations[language] = {
           {
