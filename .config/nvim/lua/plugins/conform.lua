c7bfafff7d2a49d5d533259a96ac9c6ea981e7f3
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-07-09 01:27:38 +0200

    add more fts for prettierd

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,16 +4,18 @@ return {
   event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
   opts = {
     formatters_by_ft = {
+      blade = { "blade-formatter" },
       css = { "prettierd" },
       html = { "prettierd" },
       javascript = { "prettierd" },
       json = { "prettierd" },
+      jsonc = { "prettierd" },
       json5 = { "prettierd" },
       lua = { "stylua" },
+      markdown = { "prettierd" },
       php = { "pint" },
-      blade = { "blade-formatter" },
-      vue = { "prettierd" },
       typescript = { "prettierd" },
+      vue = { "prettierd" },
       yaml = { "prettierd" },
     },
   },
